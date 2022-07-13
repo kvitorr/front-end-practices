7f6a951db1519e92f4533de2b7aea11dbb907696
Vitor Araujo <101069966+vitor-0@users.noreply.github.com>
2022-07-13 01:46:39 -0300

    calculadora basic

---
+++ javascript-dom/calculadora/script.js
@@ -0,0 +1,28 @@
+let visor = document.getElementById('visor') /*selecionar o visor*/
+
+let resultado = document.querySelector('#grid-equal-item') //seleciona o botão de igual
+
+function insert(value){
+    visor.innerHTML += value
+}
+
+function resetar(){
+    visor.innerHTML = ""
+    resultado = ''
+}
+
+function deletar(){
+
+    let novo_conteudo = ''
+    const string = visor.innerHTML
+
+    for (let i = 0; i < (string.length - 1); i++) {
+        novo_conteudo += string[i]    
+    }
+
+     visor.innerHTML = novo_conteudo
+}
+
+function calculo(){
+    visor.innerHTML = eval(visor.innerHTML)
+}
